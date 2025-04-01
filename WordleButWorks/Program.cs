01181e66f65b9e0b4fe3f5ed225b0cb4b8b3da5c
Tinh Nguyen <22TNguyen@heckgrammar.co.uk>
2025-04-01 16:35:17 +0100

    finished

--- WordleButWorks/Program.cs
+++ WordleButWorks/Program.cs
@@ -8,13 +8,39 @@ namespace WordleButWorks
         {
             Random random = new Random();
 
-            string[] Words = { };
+            string[] Words = {"apple", "brave", "chair", "dream", "eagle", "frost", "grape", "happy", "ideal", "joker",
+            "karma", "lunar", "magic", "noble", "ocean", "piano", "quiet", "raven", "solar", "tiger",
+            "ultra", "vivid", "waste", "xenon", "yacht", "zebra", "beach", "cloud", "drink", "early",
+            "fancy", "giant", "house", "input", "jolly", "kneel", "light", "mango", "novel", "orbit",
+            "peace", "quick", "robot", "sandy", "table", "unity", "value", "watch", "xerox", "youth",
+            "zesty", "amber", "brisk", "crisp", "dance", "eager", "flame", "glove", "honey", "icing",
+            "jumbo", "knock", "lemon", "mirth", "nylon", "olive", "penny", "quirk", "roast", "spend",
+            "tulip", "usage", "vowel", "wrist", "xylan", "yield", "zonal", "admin", "blend", "charm",
+            "doubt", "event", "frost", "grasp", "honor", "image", "joint", "knees", "logic", "marry",
+            "notch", "overt", "plumb", "quest", "relay", "shout", "truce", "umbra", "verge", "widen",
+            "xenon", "young", "zebra", "acorn", "board", "crown", "dwell", "exalt", "flock", "gloom",
+            "hatch", "ivory", "jumpy", "kneel", "leech", "motto", "nerdy", "onion", "plaza", "quirk",
+            "risky", "scarf", "torso", "usher", "venue", "whale", "xenon", "yearn", "zonal", "amuse",
+            "brisk", "chose", "drape", "elbow", "flick", "grind", "hover", "ingot", "jewel", "kiosk",
+            "layer", "mirth", "nudge", "ounce", "pride", "quilt", "rover", "siren", "track", "upset",
+            "vixen", "waver", "xylem", "youth", "zesty", "azure", "brass", "cider", "ditch", "error",
+            "feast", "glory", "haste", "irony", "juice", "koala", "latch", "mango", "niece", "orbit",
+            "plush", "quart", "rifle", "sloth", "torch", "umbra", "vivid", "witty", "xenon", "yeast",
+            "zonal", "angel", "bloom", "crane", "dizzy", "equip", "flute", "gravy", "hound", "input",
+            "jolly", "kneel", "lemon", "mirth", "nylon", "olive", "penny", "quirk", "roast", "spend",
+            "tulip", "usage", "vowel", "wrist", "xylan", "yield", "zonal", "admin", "blend", "charm",
+            "raven", "steed", "trust", "upend", "vigor", "wreak", "xeric", "yogic", "zebra", "brood",
+            "chaos", "drown", "evade", "flint", "grasp", "harsh", "index", "joust", "knack", "leaky",
+            "merit", "noble", "oasis", "prank", "quash", "risky", "slope", "truly", "ulcer", "vault",
+            "wharf", "xenon", "yeast", "zonal", "afoul", "birch", "cloud", "dozen", "equip", "fjord",
+            "giddy", "hence", "inlay", "jolly", "knurl", "lucid", "mirth", "north", "oxide", "proud",
+            "quill", "rifle", "scorn", "tryst", "under", "vexed", "wryly", "xenon", "yacht", "zebra"};
 
             int RandomInt = random.Next(0, 99);
             bool found = false;
-            //string Word = Words[RandomInt];
+            string Word = Words[RandomInt];
             int i = 1;
-            string Word = "array";
+            Word = "array";
             
 
             Console.WriteLine("Welcome to Wordle! Please guess our random 5 letter word. If you guessed a letter correctly and it is in the right place, a 'G' will appear under it. If it is correct, but it is in the wrong place an 'A' will appear under it. Otherwise, there will be an X.");
