7f245255d69ff5ee11a2ae71f2c16cf239e5530b
Tinh Nguyen <22TNguyen@heckgrammar.co.uk>
2025-05-21 16:53:03 +0100

    fweaefawewfweawefa

--- WordleButWorks/Program.cs
+++ WordleButWorks/Program.cs
@@ -1,47 +1,47 @@
-﻿using System.Runtime.Serialization.Formatters;
+﻿using MySqlConnector;
+using System.Runtime.Serialization.Formatters;
 
 namespace WordleButWorks
 {
     internal class Program
     {
+        static MySqlConnection connection = new MySqlConnection("server=77.37.35.4;uid=u381396247_Harvie;pwd=U|TIL&a8|R;database=u381396247_Harvie");
         static void Main(string[] args)
+
         {
-            Random random = new Random();
-
-            string[] Words = {"apple", "brave", "chair", "dream", "eagle", "frost", "grape", "happy", "ideal", "joker",
-            "karma", "lunar", "magic", "noble", "ocean", "piano", "quiet", "raven", "solar", "tiger",
-            "ultra", "vivid", "waste", "xenon", "yacht", "zebra", "beach", "cloud", "drink", "early",
-            "fancy", "giant", "house", "input", "jolly", "kneel", "light", "mango", "novel", "orbit",
-            "peace", "quick", "robot", "sandy", "table", "unity", "value", "watch", "xerox", "youth",
-            "zesty", "amber", "brisk", "crisp", "dance", "eager", "flame", "glove", "honey", "icing",
-            "jumbo", "knock", "lemon", "mirth", "nylon", "olive", "penny", "quirk", "roast", "spend",
-            "tulip", "usage", "vowel", "wrist", "xylan", "yield", "zonal", "admin", "blend", "charm",
-            "doubt", "event", "frost", "grasp", "honor", "image", "joint", "knees", "logic", "marry",
-            "notch", "overt", "plumb", "quest", "relay", "shout", "truce", "umbra", "verge", "widen",
-            "xenon", "young", "zebra", "acorn", "board", "crown", "dwell", "exalt", "flock", "gloom",
-            "hatch", "ivory", "jumpy", "kneel", "leech", "motto", "nerdy", "onion", "plaza", "quirk",
-            "risky", "scarf", "torso", "usher", "venue", "whale", "xenon", "yearn", "zonal", "amuse",
-            "brisk", "chose", "drape", "elbow", "flick", "grind", "hover", "ingot", "jewel", "kiosk",
-            "layer", "mirth", "nudge", "ounce", "pride", "quilt", "rover", "siren", "track", "upset",
-            "vixen", "waver", "xylem", "youth", "zesty", "azure", "brass", "cider", "ditch", "error",
-            "feast", "glory", "haste", "irony", "juice", "koala", "latch", "mango", "niece", "orbit",
-            "plush", "quart", "rifle", "sloth", "torch", "umbra", "vivid", "witty", "xenon", "yeast",
-            "zonal", "angel", "bloom", "crane", "dizzy", "equip", "flute", "gravy", "hound", "input",
-            "jolly", "kneel", "lemon", "mirth", "nylon", "olive", "penny", "quirk", "roast", "spend",
-            "tulip", "usage", "vowel", "wrist", "xylan", "yield", "zonal", "admin", "blend", "charm",
-            "raven", "steed", "trust", "upend", "vigor", "wreak", "xeric", "yogic", "zebra", "brood",
-            "chaos", "drown", "evade", "flint", "grasp", "harsh", "index", "joust", "knack", "leaky",
-            "merit", "noble", "oasis", "prank", "quash", "risky", "slope", "truly", "ulcer", "vault",
-            "wharf", "xenon", "yeast", "zonal", "afoul", "birch", "cloud", "dozen", "equip", "fjord",
-            "giddy", "hence", "inlay", "jolly", "knurl", "lucid", "mirth", "north", "oxide", "proud",
-            "quill", "rifle", "scorn", "tryst", "under", "vexed", "wryly", "xenon", "yacht", "zebra"};
-
             Console.ForegroundColor = ConsoleColor.DarkGray;
 
-            int RandomInt = random.Next(0, 99);
+            Random random = new Random();
+
+            MySqlCommand wordCount = new MySqlCommand();
+            string wordCountString = $"SELECT COUNT(*) FROM WordData";
+            wordCount.Connection = connection;
+            wordCount.CommandText = wordCountString;
+            connection.Open();
+            MySqlDataReader reader = wordCount.ExecuteReader();
+            reader.Read();
+            int count = reader.GetInt32(0);
+            connection.Close();
+
+            int RandomInt = random.Next(0, count);
+
+            MySqlCommand getWords = new MySqlCommand();
+            string getWordsString = $"SELECT `word` FROM WordData WHERE `wordID` = {RandomInt}";
+            getWords.Connection = connection;
+            getWords.CommandText = getWordsString;
+            connection.Open();
+            reader = getWords.ExecuteReader();
+            reader.Read();
+            string Word = reader.GetString(0);
+            connection.Close();
+
+            
+
             bool found = false;
-            string Word = Words[RandomInt];
             int i = 1;
+
+            Console.WriteLine(Word);
+            
             //Word = "array";
             
 
@@ -59,7 +59,7 @@ namespace WordleButWorks
                 while ((valid == false))
                 {
                     Console.WriteLine($"Please enter guess number {i}");
-                    Response = Console.ReadLine().ToLower();
+                    Response = Console.ReadLine().ToUpper();
 
                     if (Response.Length == 5)
                     {
