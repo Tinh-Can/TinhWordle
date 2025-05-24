8158e51625f5a1ff2abdfb9c50a8ab7cc7e3a6e2
Tinh Can <22tnguyen@heckgrammar.co.uk>
2025-05-24 08:46:44 +0100

    Update Program.cs
    HEHEHE

--- WordleButWorks/Program.cs
+++ WordleButWorks/Program.cs
@@ -1,5 +1,6 @@
-﻿using MySqlConnector;
+using MySqlConnector;
 using System.Runtime.Serialization.Formatters;
+using System;
 
 namespace WordleButWorks
 {
@@ -9,126 +10,187 @@ namespace WordleButWorks
         static void Main(string[] args)
 
         {
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-
-            Random random = new Random();
-
-            MySqlCommand wordCount = new MySqlCommand();
-            string wordCountString = $"SELECT COUNT(*) FROM WordData";
-            wordCount.Connection = connection;
-            wordCount.CommandText = wordCountString;
-            connection.Open();
-            MySqlDataReader reader = wordCount.ExecuteReader();
-            reader.Read();
-            int count = reader.GetInt32(0);
-            connection.Close();
-
-            int RandomInt = random.Next(0, count);
-
-            MySqlCommand getWords = new MySqlCommand();
-            string getWordsString = $"SELECT `word` FROM WordData WHERE `wordID` = {RandomInt}";
-            getWords.Connection = connection;
-            getWords.CommandText = getWordsString;
-            connection.Open();
-            reader = getWords.ExecuteReader();
-            reader.Read();
-            string Word = reader.GetString(0);
-            connection.Close();
-
-            
-
-            bool found = false;
-            int i = 1;
-
-            Console.WriteLine(Word);
-            
-            //Word = "array";
-            
-
-            Console.WriteLine("Welcome to Wordle! Please guess our random 5 letter word. If you guessed a letter correctly and it is in the right place, a 'G' will appear under it. If it is correct, but it is in the wrong place an 'A' will appear under it. Otherwise, there will be an X.");
-
-            while ((found == false)&&(i<=6))
+            void ClearConsoleLine(int line)
             {
-                int place = 0;
-                int correct = 0;
-                string[] Aplace = { " ", " ", " ", " ", " " };
-                string[] Acorrect = { " ", " ", " ", " ", " " };
-                bool valid = false;
-                string Response = "     ";
+                line = line - 1;
+                int currentLineCursor = Console.CursorTop;
+                Console.SetCursorPosition(0, line);
+                Console.Write(new string(' ', Console.BufferWidth)); // Use BufferWidth, not WindowWidth
+                Console.SetCursorPosition(0, currentLineCursor);
+            }
 
-                while ((valid == false))
+            void DeleteLastConsoleLine()
+            {
+                if (Console.CursorTop == 0)
                 {
-                    Console.WriteLine($"Please enter guess number {i}");
-                    Response = Console.ReadLine().ToUpper();
-
-                    if (Response.Length == 5)
-                    {
-                        valid = true;
-                    } else
-                    {
-                        Console.WriteLine("Invalid Input. 5 Letters Please");
-                    }
+                    return; // Don't go above line 0 or you'll summon a segmentation fault from the void
                 }
+                int previousLine = Console.CursorTop - 1;
+                Console.SetCursorPosition(0, previousLine);
+                Console.Write(new string(' ', Console.BufferWidth)); // Overwrite with spaces
+                Console.SetCursorPosition(0, previousLine); // Move cursor to where the deleted line was
+            }
 
-                for (int j = 0; j < 5; j++)
-                {
-                    for (int k = 0; k < 5; k++)
-                    {
-                        if (Word[j] == Response[k])
-                        {
-                            if (j == k)
-                            {
-                                correct++;
-                                Acorrect[k] = "G";
-                            } else
-                            {
-                                place++;
-                                Aplace[k] = "G";
-                            }
-                        }
-                    }
-                }
+            int won = 0;
 
-                if (correct == 5)
+            bool play = true;
+
+            while (play == true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                Random random = new Random();
+
+                MySqlCommand wordCount = new MySqlCommand();
+                string wordCountString = $"SELECT COUNT(*) FROM WordData";
+                wordCount.Connection = connection;
+                wordCount.CommandText = wordCountString;
+                connection.Open();
+                MySqlDataReader reader = wordCount.ExecuteReader();
+                reader.Read();
+                int count = reader.GetInt32(0);
+                connection.Close();
+
+                int RandomInt = random.Next(0, count);
+
+                MySqlCommand getWords = new MySqlCommand();
+                string getWordsString = $"SELECT `word` FROM WordData WHERE `wordID` = {RandomInt}";
+                getWords.Connection = connection;
+                getWords.CommandText = getWordsString;
+                connection.Open();
+                reader = getWords.ExecuteReader();
+                reader.Read();
+                string Word = reader.GetString(0);
+                connection.Close();
+
+
+
+                bool found = false;
+                int i = 1;
+
+                //Console.WriteLine(Word);
+
+                //Word = "array";
+
+                Console.Clear();
+                Console.WriteLine("Welcome to Wordle! Please enter anything to continue.");
+                Console.ReadLine();
+                Console.Clear();
+
+                int repeater = 1;
+
+                while ((found == false) && (i <= 6))
                 {
-                    found = true;
-                } else
-                {
-                    Console.Clear();
-                    for (int l = 0; l < 5; l++)
+                    int place = 0;
+                    int correct = 0;
+                    string[] Aplace = { " ", " ", " ", " ", " " };
+                    string[] Acorrect = { " ", " ", " ", " ", " " };
+                    bool valid = false;
+                    string Response = "     ";
+
+                    
+                    while ((valid == false))
                     {
-                        if (Acorrect[l] == "G")
+                        Console.WriteLine($"Please enter guess number {i}");
+                        Response = Console.ReadLine().ToUpper();
+                        DeleteLastConsoleLine();
+                        DeleteLastConsoleLine();
+
+
+                        if (Response.Length == 5)
                         {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.Write(Response[l]);
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
-                        }
-                        else if (Aplace[l] == "G")
-                        {
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.Write(Response[l]);
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            valid = true;
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
-                            Console.Write(Response[l]);
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.WriteLine("Invalid Input. Please wait a few seconds.");
+                            Thread.Sleep(1000);
+                            DeleteLastConsoleLine();
                         }
                     }
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        for (int k = 0; k < 5; k++)
+                        {
+                            if (Word[j] == Response[k])
+                            {
+                                if (j == k)
+                                {
+                                    correct++;
+                                    Acorrect[k] = "G";
+                                }
+                                else
+                                {
+                                    place++;
+                                    Aplace[k] = "G";
+                                }
+                            }
+                        }
+                    }
+
+                    if (correct == 5)
+                    {
+                        found = true;
+                    }
+                    //else
+                    //{
+                        
+                        for (int l = 0; l < 5; l++)
+                        {
+                            if (Acorrect[l] == "G")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(Response[l]);
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                            }
+                            else if (Aplace[l] == "G")
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.Write(Response[l]);
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                                Console.Write(Response[l]);
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                            }
+                        }
+                    //}
+
+                    Console.WriteLine(" ");
+                    i++;
                 }
 
-                Console.WriteLine(" ");
-                i++;
-            }
+                if (found == true)
+                {
 
-            if (found == true)
-            {
-                Console.WriteLine($"You win! The correct word was: {Word}");
-            }
-            else
-            {
-                Console.WriteLine($"You have run out of guesses! The correct word was: {Word}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    //Console.Clear();
+                    Console.WriteLine($"You win! The correct word was: {Word}");
+                    won++;
+                    Console.WriteLine($"You have won a total of {won} games.");
+                    Console.WriteLine("Would you like to play again? Input 'Y' for yes, or type any other key for no.");
+                    string PlayAgain = Console.ReadLine();
+                    if (PlayAgain != "Y")
+                    {
+                        play = false;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    //Console.Clear();
+                    Console.WriteLine($"You have run out of guesses! The correct word was: {Word}");
+                    Console.WriteLine($"You have won a total of {won} games.");
+                    Console.WriteLine("Would you like to play again? Input 'Y' for yes, or type any other key for no.");
+                    string PlayAgain = Console.ReadLine();
+                    if (PlayAgain != "Y")
+                    {
+                        play = false;
+                    }
+                }
             }
 
         }
